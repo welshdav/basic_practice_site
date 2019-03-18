5705f860f79f10ea5615056e208efc6574d67f4c
David Welsh <welshdav@msu.edu>
2019-03-18 12:34:25 -0500

    Create main.js

---
+++ jquery_review/event_practice/main.js
@@ -0,0 +1,16 @@
+$(document).ready(() =>{
+  $('#cart').on('click', () =>{
+    $('#cartMenu').show();
+  });
+   $('#account').on('click', () =>{
+    $('#accountMenu').show(); 
+  });
+   $('#help').on('click', () =>{
+    $('#helpMenu').show(); 
+  });
+  
+  	$('.dropdown-menu').on('mouseleave', () =>{
+      $('.dropdown-menu').hide()
+    });
+  
+})
