db7971eb9b83b5fb294fd05924cd324a97a37286
David Welsh <welshdav@msu.edu>
2019-03-18 16:13:55 -0500

    Create main.js

---
+++ jquery_review/blog_post/main.js
@@ -0,0 +1,29 @@
+$(document).ready(() => {
+	$('.menu').on('mouseenter', () => {
+    $('.nav-menu').show();
+  }); 
+  
+  $('.nav-menu').on('mouseleave', () => {
+    $('.nav-menu').hide(); 
+  }); 
+  
+  $('btn').on('mouseenter', () => {
+    $(event.currentTarget).addClass('btn-hover').on('mouseleave', (event) => {$(event.currentTarget).removeClass('btn-hover')
+    });
+  }); 
+  
+  $('postText').on('keyup', (event) => {
+    let post = $(event.currentTarget).val(); 
+    let remaining = 140 - post.length; 
+    
+    if (remaining <= 0){
+      $('wordCount').addClass('red');
+    } else{
+      $('wordCount').removeClass('red');
+    };
+    
+    $('.characters').html(remaining);
+  }); 
+  
+  $('postText').focus()
+}) 
