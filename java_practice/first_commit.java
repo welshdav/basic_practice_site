89301b439917097ada6c299478bb13be618f21d6
David Welsh <welshdav@msu.edu>
2019-04-03 07:42:41 -0500

    Create first_commit.java

---
+++ java_practice/first_commit.java
@@ -0,0 +1,8 @@
+// Define your tree class in this file!
+public class Tree{
+ public static void main(String[] args) {
+   //Am I overburdening myself learning so many languages?
+   //Probably...
+   System.out.print.ln("This is why I used printLn in that interview insert laughing emoji")
+ } 
+}
