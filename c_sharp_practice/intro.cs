d66135b8f47630724650e2bc2b5c7895d0a14ec7
David Welsh <welshdav@msu.edu>
2019-04-08 16:00:13 -0500

    Create intro.cs

---
+++ c_sharp_practice/intro.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //This is going to be intersting...
+      Console.WriteLine(" c__    ___"); 
+      Console.WriteLine("    \\__|__|___"); 
+      Console.WriteLine("      \(o_o)");     
+      Console.WriteLine("        >~  >"); 
+      Console.WriteLine("       /   / \\"); 
+      Console.WriteLine("      /   /   \\");
+      Console.WriteLine("      \   )   c/"); 
+      Console.WriteLine("      /   /"); 
+      Console.WriteLine("     /   /|");
+      Console.WriteLine("    (    (  \ ");
+      Console.WriteLine("    |    |"); 
+      Console.WriteLine("    | O |"); 
+      
+      Console.WriteLine("I recognize the need to do this but"); 
+//This is should almost go in the fool's code repository...
+     }
+  }
+}
