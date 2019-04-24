74300fa0674d559482ab9d80a7752353d90f7865
David Welsh <welshdav@msu.edu>
2019-04-24 12:35:33 -0500

    Create age_calc.js

---
+++ rudimentary_jscript_refresher/age_calc.js
@@ -0,0 +1,11 @@
+const myAge = 25; 
+// not exactly
+var earlyYears = 2; 
+//I love refereshers...
+earlyYears *= 10.5; 
+
+let laterYears = myAge - 2;
+laterYears *= 4; 
+
+console.log(laterYears); 
+console.log(earlyYears); 
