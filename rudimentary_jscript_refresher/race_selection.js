cd17407ac9575201daed0b99b82ffb09b1ef8ec5
David Welsh <welshdav@msu.edu>
2019-04-25 15:32:32 -0500

    Rename num_selection.js to race_selection.js

---
+++ rudimentary_jscript_refresher/race_selection.js
@@ -0,0 +1,10 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+const registeredEarly = false; 
+const age = 27; 
+
+if( age >= 18 && registeredEarly === true){
+  console.log(raceNumber += 1000); 
+}else{
+  console.log(raceNumber);
+}
